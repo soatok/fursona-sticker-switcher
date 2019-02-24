fe0627af14a63734eec626f2fe83a9282990ba27
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-02-23 23:55:54 -0500

    Minor nits

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -108,6 +108,3 @@ app.on('activate', function () {
         createWindow()
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
