bb9067afb98c7dba494177115816f1232f3f340f
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-03-03 00:51:19 -0500

    Cleanup

--- packaging/build.sh
+++ packaging/build.sh
@@ -6,7 +6,7 @@ echo "Building releases..."
 parallel ::: "./build-linux.sh" "./build-mac.sh" "./build-windows.sh"
 echo "Done. Now to sign each release zip..."
 for filename in *.zip; do
-    [ -e "$filename" ] || continue
+    [[ -e "$filename" ]] || continue
     gpg --output "${filename}.sig" --detach-sig "${filename}"
 done
 echo "Done."
